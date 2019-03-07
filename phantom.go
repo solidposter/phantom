fd25935c4c9d17ed39577ef053a1c0096f47f35e
Tony Sarendal <tony@polarcap.org>
2019-03-07 06:16:48 +0100

    Fix flags.Usage(), add small manual.

--- phantom.go
+++ phantom.go
@@ -43,6 +43,7 @@ func main() {
 	pktsPtr := flag.Int("c", 1000, "number of packets to send per client")
 	sizePtr := flag.Int("b", 512, "packet size")
 	rampPtr := flag.Int("r", 0, "Ramp-up interval in seconds")
+	flag.Usage = usage
 	flag.Parse()
 
 	// catch CTRL+C
@@ -230,5 +231,42 @@ func udpclient(addr string, numpkts int, pktsize int, key int) {
 			atomic.AddUint64(&totDrops, 1)
 		}
 	}
+}
+
+func usage() {
+	const text = `
+Run as a server (server port is the final option):
+
+server options:
+ -s            enable server mode
+ -k <number>   server key
+
+phantom -s (random key and port are chosen)
+phantom -k 1969 -s (server key 1969, random port)
+phantom -k 1969 -s 2929 (server key 1969, port 2929)
+
+
+Run as a client (server ip:port is the final option):
+
+client options:
+ -n <number>   number of threads (default 1)
+ -c <number>   packets per thread (default 1k)
+ -b <number>   packet size (default 512 bytes)
+ -k <number>   server key
+ -r <number>   ramp-up mode (see below)
+
+phantom -k 1969 192.0.2.1:2929 (key 1969 server 192.0.2.1:2929)
+phantom -k 1969 -n 10 -c 1000000 -b 288 192.0.2.1:2929 (10 threads, 100k packets per thread, 288 byte packets)
+
+Set -c 0 to get a client that runs for a very long time, or til interrupted.
+
+Client in ramp-up mode:
+
+In ramp-up mode the client will add a thread at the interval specified until packet loss is detected.
+The option -r overrides the -n and -c options.
+
+phantom -k 1969 -r 5 192.0.2.1:2929 (key 1969, ramp-up 5s, server 192.0.2.1:2929)
+`
+	fmt.Print(text)
 }
 
